3c49250b4588ce9b55356dc175c6de1a04a78ba0
the forgotten yogurt <65128443+200-yogurt@users.noreply.github.com>
2026-06-13 16:45:27 -0500

    Minor tweaks to utils.py

--- Projects/RPG Prototype/Systems/utils.py
+++ Projects/RPG Prototype/Systems/utils.py
@@ -6,13 +6,15 @@ def narrate(text, amount):
     time.sleep(amount)
 def intro_cutscene(type):
     if "new" in type:
-        narrate("\n- WELCOME . . . TO THIS EXCITING NEW ADVENTURE \n", speed["moderate"])
-        narrate("- BEFORE WE CONTINUE, I'D LIKE TO KNOW MORE ABOUT YOU...", speed["moderate"])
-        narrate("Loading. . .", speed["slow"])
 
-        playerData["NAME"] = input("* * Type your username: * *\n")
+        narrate("\n- WELCOME . . . TO THIS EXCITING NEW ADVENTURE \n", speed["moderate"])
+        narrate("- BEFORE WE CONTINUE, I'D LIKE TO KNOW MORE ABOUT YOU...", speed["slow"])
+        narrate("Loading. . .", speed["fast"])
+
+        playerData["stats"]["NAME"] = input("* * Type your username: * *\n")
 
         narrate("\n     EXCELLENT . . . \n- SHALL WE CONTINUE?", speed["slow"])
     elif "old" in type:
-        print(f"- WELCOME BACK, {playerData["NAME"]}") 
+        
+        print(f"- WELCOME BACK, {playerData["stats"]["NAME"]}") 
         narrate("Loading. . .", speed["slow"])
